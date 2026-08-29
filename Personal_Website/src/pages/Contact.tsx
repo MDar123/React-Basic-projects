8528fa47900f19703b334137ee534b83a8443716
MDar123 <mujtabadar98@gmail.com>
2026-08-29 10:16:08 -0700

    updated env

--- Personal_Website/src/pages/Contact.tsx
+++ Personal_Website/src/pages/Contact.tsx
@@ -21,14 +21,14 @@ const Contact = () => {
   const onSubmit = async (data: FormData) => {
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,      
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,     
+        import.meta.env.EMAILJS_SERVICE_ID,
+        import.meta.env.EMAILJS_TEMPLATE_ID,
         {
           from_name: data.name,
           reply_to: data.email,
           message: data.message,
         },
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY       
+        import.meta.env.EMAILJS_PUBLIC_KEY
       );
 
       toast.success('Message sent successfully!');
